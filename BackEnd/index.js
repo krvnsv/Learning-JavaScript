68f9d1e95c17204499db6ba6a47942e26cec1693
Kirill <khanov.krl@gmail.com>
2024-12-17 20:59:45 +0100

    Tryiyng node

---
+++ BackEnd/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+console.log(global.luckyNum);
+
+global.luckyNum = '23';
+
+console.log(global.luckyNum);
+
+console.log(process.platform);
+
+console.log(process.env.USER);
